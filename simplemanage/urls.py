7531d12f15fd94f614fbf4fda8708f2618662e30
zhhan <zhhan@niub.la>
2018-05-18 18:25:38 +0800

    add paging function

--- simplemanage/urls.py
+++ simplemanage/urls.py
@@ -20,6 +20,7 @@ from app01 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', views.Login.as_view()),
+    path('', views.Classes.as_view()),
     path('classes/', views.Classes.as_view()),
     path('logout/', views.Logout.as_view()),
 ]
